0de2c829421121810b61f759032839587d7826a9
yashgandhi32 <yash@sendinblue.com>
2020-07-25 20:38:34 +0530

    delete task

--- db/tasks.go
+++ db/tasks.go
@@ -41,6 +41,36 @@ func CreateTask(task string) (int, error) {
 	return id64, nil
 }
 
+func AllTasks() ([]Task, error) {
+	var tasks []Task
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: string(v),
+			})
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func DeleteTask(key int) error {
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		return b.Delete(itob(key))
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func itob(val int) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, uint64(64))
