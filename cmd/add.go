2c2bd9b6de858621fb8cdbea45dfacdcb8869c13
yashgandhi32 <yash@sendinblue.com>
2020-07-25 20:46:04 +0530

    list, add task commands created

--- cmd/add.go
+++ cmd/add.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/yashgandhi-32/testprojects/cli-taskmanager/db"
 )
 
 var addCmd = &cobra.Command{
@@ -12,7 +13,11 @@ var addCmd = &cobra.Command{
 	Short: "Adds a task to your task list.",
 	Run: func(cmd *cobra.Command, args []string) {
 		task := strings.Join(args, " ")
-		fmt.Printf("Added \"%s\" to your task list.\n", task)
+		_, err := db.CreateTask(task)
+		if err != nil {
+			fmt.Println("SOmething went wrong to add task")
+			return
+		}
 	},
 }
 
--- cmd/list.go
+++ cmd/list.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/yashgandhi-32/testprojects/cli-taskmanager/db"
 )
 
 // listCmd represents the list command
@@ -12,7 +14,19 @@ var listCmd = &cobra.Command{
 	Short: "A brief description of your command",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Print("Something went wrong")
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Print("You have no task")
+			return
+		}
+		fmt.Print("You have following task")
+		for i, task := range tasks {
+			fmt.Printf("%d . %s\n", i+1, task.Value)
+		}
 	},
 }
 
